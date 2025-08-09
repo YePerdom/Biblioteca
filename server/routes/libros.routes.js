3da92f56d5923fba7e21ba42f5f9304d2cd6bc06
Yoelmis Enrique Perdomo Pardo <yoepardo@gmail.com>
2025-08-09 16:40:31 -0500

    eliminacion
    eliminacion de archivos para la reestructuracion de proyecto

--- server/routes/libros.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { librosMasPrestados } from "../controllers/libros.controllers.js";
-
-const librosRouter = Router();
-
-librosRouter.get('/mas-prestados', librosMasPrestados);
-
-export default librosRouter;
--- server/routes/usuarios.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { usuariosRetrasaados } from "../controllers/usuarios.controllers.js";
-
-const usuariosRouter = Router();
-
-usuariosRouter.get('/con-retrasos', usuariosRetrasaados);
-
-export default usuariosRouter;
