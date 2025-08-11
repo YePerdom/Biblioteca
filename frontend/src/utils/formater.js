35060285be7c1da4c9bea35f40b2df18ecc161a1
Yoelmis Enrique Perdomo Pardo <yoepardo@gmail.com>
2025-08-10 20:12:37 -0500

    modificacion de componentes
    se cambio el nombre para

--- frontend/src/utils/formater.js
+++
@@ -1,8 +0,0 @@
-export function formatearFecha(fechaIso) {
-    const fecha = new Date(fechaIso);
-    return fecha.toLocaleDateString("es-Co", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric"
-    });
-};
